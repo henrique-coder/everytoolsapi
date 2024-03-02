bdb37dd46cb0ad489c3400b41ce58a7b0cd32047
Henrique <henriquemoreira10fk@gmail.com>
2024-03-02 18:52:30 -0300

    `add` url-generator/mediafire-file route to api

---
+++ api_resources/main_endpoints/url_generator/v1/mediafire_file.py
@@ -0,0 +1,26 @@
+from typing import Union
+from httpx import get, _exceptions as httpx_exceptions
+from lxml import html
+
+
+def main(_id: str) -> Union[str, None]:
+    url = f'https://www.mediafire.com/file/{_id}'
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+    }
+
+    try:
+        resp = get(url, headers=headers, timeout=5)
+    except httpx_exceptions.HTTPError:
+        return None
+
+    try:
+        tree = html.fromstring(resp.content)
+        data = tree.xpath('//a[@id="downloadButton"]/@href')[0]
+        data = str(data[:data.rfind('/')])
+    except Exception:
+        return None
+
+    return data
