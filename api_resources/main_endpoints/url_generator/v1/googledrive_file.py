0a32073d0e92773f04ee5ca0ad7bf248254a6557
Henrique <henriquemoreira10fk@gmail.com>
2024-03-02 18:56:52 -0300

    `add` url-generator/googledrive-file route to api

---
+++ api_resources/main_endpoints/url_generator/v1/googledrive_file.py
@@ -0,0 +1,25 @@
+from typing import Union
+from httpx import get, _exceptions as httpx_exceptions
+from lxml import html
+
+
+def main(_id: str) -> Union[str, None]:
+    url = f'https://drive.google.com/uc?export=download&id={_id}'
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+    }
+
+    try:
+        resp = get(url, headers=headers, timeout=5)
+    except httpx_exceptions.HTTPError:
+        return None
+
+    try:
+        tree = html.fromstring(resp.content)
+        data = tree.xpath('//form[@id="download-form"]/@action')[0]
+    except Exception:
+        data = url
+
+    return data
