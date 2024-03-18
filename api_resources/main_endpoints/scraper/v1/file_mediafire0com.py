5850bdc194a33673eff0231b84521aad5ff1098e
Henrique <henriquemoreira10fk@gmail.com>
2024-03-18 19:31:44 -0300

    `optimized` endpoint py code

--- api_resources/main_endpoints/scraper/v1/file_mediafire0com.py
+++ api_resources/main_endpoints/scraper/v1/file_mediafire0com.py
@@ -1,25 +1,33 @@
 from typing import Union
 from httpx import get, _exceptions as httpx_exceptions
 from lxml import html
+from time import perf_counter
 
 
-def main(_id: str) -> Union[str, None]:
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+}
+
+
+def main(_id: str) -> Union[dict, None]:
+    start_time = perf_counter()
+    generated_data = dict()
+
     url = f'https://www.mediafire.com/file/{_id}'
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
-    }
 
     try:
         resp = get(url, headers=headers, timeout=5)
+        resp.raise_for_status()
     except httpx_exceptions.HTTPError:
         return None
 
     try:
         tree = html.fromstring(resp.content)
         data = tree.xpath('//a[@id="downloadButton"]/@href')[0]
-        data = str(data[:data.rfind('/')])
+        generated_data['url'] = str(data[:data.rfind('/')])
     except Exception:
         return None
 
-    return data
+    generated_data['processing_time'] = float(perf_counter() - start_time)
+
+    return generated_data
