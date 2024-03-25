3daa2b47965b8bc3d3d60daea1568d2033a6dede
Henrique <henriquemoreira10fk@gmail.com>
2024-03-25 13:27:03 -0300

    `enchanced` sorting scrapers

--- api_resources/main_endpoints/scraper/v1/product_promotions.py
+++ api_resources/main_endpoints/scraper/v1/product_promotions.py
@@ -1,7 +1,10 @@
-from typing import Union
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from httpx import get, _exceptions as httpx_exceptions
 from bs4 import BeautifulSoup
-from time import perf_counter
+from urllib.parse import unquote
+from time import perf_counter, sleep
+from typing import Union
 
 
 headers = {
@@ -12,33 +15,51 @@ main_promotion_websites = {
     'boletando.com': 'https://boletando.com/?s={}&asp_active=1&p_asid=1',
 }
 
+selenium_options = Options()
+selenium_options.headless = True
+
 
 def main(_name: str) -> Union[dict, None]:
     start_time = perf_counter()
     generated_data = {'data': {'active_promotions': dict(), 'scraped_websites': list()}}
 
     for website, url in main_promotion_websites.items():
-        try:
-            resp = get(url.format(_name), headers=headers, timeout=5)
-            resp.raise_for_status()
-        except httpx_exceptions.HTTPError:
-            continue
+        def get_html_source_by_get_request(_url: str) -> Union[str, None]:
+            try:
+                resp = get(_url, headers=headers, timeout=5)
+                resp.raise_for_status()
+                return resp.text
+            except httpx_exceptions.HTTPError:
+                return None
 
-        generated_data['data']['scraped_websites'].append(website)
-
-        soup = BeautifulSoup(resp.text, 'html.parser')
+        def get_html_source_by_selenium(_url: str, sleep_time: int) -> Union[str, None]:
+            try:
+                selenium_driver = webdriver.Chrome(options=selenium_options)
+                selenium_driver.get(_url)
+                sleep(sleep_time)
+                extracted_html = selenium_driver.page_source
+                selenium_driver.quit()
+                return extracted_html
+            except Exception:
+                return None
 
         if website == 'boletando.com':
+            url = url.format(_name)
+            resp_text = get_html_source_by_get_request(url)
+            if not resp_text:
+                continue
+
+            soup = BeautifulSoup(resp_text, 'html.parser')
+
             for item in soup.find_all('div', class_='grid_desc_and_btn'):
                 product_name = item.find('h3').text.strip()
+
                 if product_name.lower().startswith('[ encerrada ]') or not item.find('span', class_='rh_regular_price'):
                     continue
 
-                generated_data['data']['active_promotions'][product_name] = {
-                    'url': item.find('a', class_='re_track_btn')['href'],
-                }
-        else:
-            return None
+                generated_data['data']['active_promotions'][product_name] = {'url': unquote(item.find('a', class_='re_track_btn')['href'])}
+
+        generated_data['data']['scraped_websites'].append(website)
 
     generated_data['processing_time'] = float(perf_counter() - start_time)
 
