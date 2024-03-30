9e2ba17ee2a834a879f5e06d022caf3798c83b09
Henrique <henriquemoreira10fk@gmail.com>
2024-03-30 15:59:38 -0300

    `fixed` critical errors on text generation and removed max_tokens api param

--- api_resources/main_endpoints/ai/v1/ask_gemini.py
+++ api_resources/main_endpoints/ai/v1/ask_gemini.py
@@ -1,49 +1,59 @@
 from typing import Union
-from httpx import get, post
-from mimetypes import guess_type
+from httpx import head, get, post
 from base64 import b64encode
 from time import perf_counter
 
 
-def main(gemini_api_keys: list, prompt: str, image_url: str = None, max_tokens: Union[int, str] = 300) -> Union[dict, None]:
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+}
+
+def main(gemini_api_keys: list, prompt: str, image_url: str = None, max_tokens: Union[str, int] = 10000) -> Union[dict, None]:
     start_time = perf_counter()
     generated_data = dict()
 
-    prompt = prompt.strip() if prompt else None
-    image_url = image_url.strip() if image_url else None
-    max_tokens = int(max_tokens) if max_tokens else 300
+    prompt = str(prompt)
+    image_url = str(image_url) if image_url else None
+    max_tokens = int(max_tokens) if max_tokens else 400
 
     def detect_mimetype(url: str) -> Union[str, None]:
         try:
-            mimetype = guess_type(url)[0]
-            return mimetype
-        except BaseException:
-            return None
-
-    def check_url_content(url: str) -> Union[bytes, None]:
-        try:
-            response = get(url, follow_redirects=True, timeout=5)
+            response = head(url, headers=headers, follow_redirects=True, timeout=5)
             response.raise_for_status()
 
             if response.status_code == 200:
-                return response.content
+                content_type = response.headers['Content-Type']
+
+                if not content_type or not str(content_type).strip():
+                    return None
+                else:
+                    return content_type.strip().lower()
             else:
                 return None
         except BaseException:
             return None
 
-    available_models = {
-        'text_only': 'gemini-pro',
-        'text_and_image': 'gemini-pro-vision'
-    }
+    def get_url_bytes(url: str) -> Union[bytes, None]:
+        try:
+            response = get(url, headers=headers, follow_redirects=True, timeout=5)
+            response.raise_for_status()
 
-    params = {
-        'key': None
-    }
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
-    }
+            if response.status_code == 200:
+                content = response.content
+
+                if content:
+                    return content
+                else:
+                    return None
+            else:
+                return None
+        except BaseException:
+            return None
+
+    available_models = {'text_only': 'gemini-pro', 'text_and_image': 'gemini-pro-vision'}
+
+    params = {'key': None}
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
     body = {
         'contents': [
             {
@@ -59,21 +69,28 @@ def main(gemini_api_keys: list, prompt: str, image_url: str = None, max_tokens: 
         }
     }
 
-    image_content = None
-    image_mime_type = None
+    allowed_image_mime_types = ['image/png', 'image/jpeg', 'image/webp', 'image/heic', 'image/heif']
+    image_mimetype = None
+    has_image = False
 
     if image_url:
-        image_content = check_url_content(image_url)
-    if image_content:
-        image_mime_type = detect_mimetype(image_url)
+        image_mimetype = detect_mimetype(image_url)
 
-    if image_mime_type:
-        model = available_models['text_and_image']
-        api_url = f'https://generativelanguage.googleapis.com/v1beta/models/{model}:generateContent'
-        body['contents'][0]['parts'].append({'inline_data': {'mime_type': image_mime_type, 'data': b64encode(image_content).decode('utf-8')}})
+        if image_mimetype in allowed_image_mime_types:
+            image_bytes = get_url_bytes(image_url)
+
+            if image_bytes:
+                has_image = True
+        elif image_url:
+            return None
+
+    if has_image:
+        used_model = available_models['text_and_image']
+        api_url = f'https://generativelanguage.googleapis.com/v1beta/models/{used_model}:generateContent'
+        body['contents'][0]['parts'].append({'inline_data': {'mime_type': image_mimetype, 'data': b64encode(image_bytes).decode('utf-8')}})
     else:
-        model = available_models['text_only']
-        api_url = f'https://generativelanguage.googleapis.com/v1beta/models/{model}:generateContent'
+        used_model = available_models['text_only']
+        api_url = f'https://generativelanguage.googleapis.com/v1beta/models/{used_model}:generateContent'
 
     try:
         attempt_number = 0
@@ -81,7 +98,7 @@ def main(gemini_api_keys: list, prompt: str, image_url: str = None, max_tokens: 
 
         while attempt_number < len(gemini_api_keys):
             params['key'] = gemini_api_keys[attempt_number]
-            response = post(api_url, params=params, headers=headers, json=body, timeout=30)
+            response = post(api_url, params=params, headers=headers, json=body, timeout=40)
 
             if response.status_code == 200:
                 break
@@ -91,7 +108,7 @@ def main(gemini_api_keys: list, prompt: str, image_url: str = None, max_tokens: 
         if response.status_code != 200:
             return None
 
-        generated_data['data'] = {'text': response.json()['candidates'][0]['content']['parts'][0]['text'], 'model': model, 'max_tokens': max_tokens}
+        generated_data['data'] = {'text': response.json()['candidates'][0]['content']['parts'][0]['text'], 'model': used_model, 'max_tokens': max_tokens}
         generated_data['processing_time'] = float(perf_counter() - start_time)
         return generated_data
     except BaseException:
