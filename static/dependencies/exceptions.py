69b0f876e31d646dca9f501e8cbcaae23c44e9df
Henrique <henriquemoreira10fk@gmail.com>
2024-05-12 20:33:35 -0300

    `added` new general exceptions

--- static/dependencies/exceptions.py
+++ static/dependencies/exceptions.py
@@ -25,3 +25,21 @@ class Exceptions:
 
     class PARAMETER_MUST_BE_GREATER_THAN:
         message = 'The parameter "{0}" must be greater than {1}.'
+
+    class INVALID_REQUEST_HEADERS:
+        message = 'The request headers are invalid. Please check the headers and try again.'
+
+    class USER_AGENT_HEADER_NOT_FOUND:
+        message = 'The "User-Agent" header was not found in the request headers.'
+
+    class ONLINE_REQUEST_FAILED:
+        message = 'This request failed, please try again or report this error at https://github.com/Henrique-Coder/everytoolsapi/issues.'
+
+    class INVALID_PUBLIC_IP_ADDRESS:
+        message = 'The IP address is invalid ({0}). Please enter a valid public IP address.'
+
+    class IPAPI_PRIVATE_RANGE:
+        message = 'The IP address is in a private range ({0}). Please enter a public IP address.'
+
+    class IPAPI_RESERVED_RANGE:
+        message = 'The IP address is in a reserved range ({0}). Please enter a public IP address.'
