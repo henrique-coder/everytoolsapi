faecc0e66884166956d9377edaa543ac4d8d24dc
Henrique <henriquemoreira10fk@gmail.com>
2024-06-13 12:21:07 -0300

    Added caching and limiting via Redis and fixed bugs in dotenv.

--- everytoolsapi.py
+++ everytoolsapi.py
@@ -1,20 +1,25 @@
 import flask
-from flask_caching import Cache
 from flask_limiter import util as flask_limiter_utils, Limiter
+from flask_caching import Cache
 from flask_talisman import Talisman
 from flask_wtf.csrf import CSRFProtect
 from flask_compress import Compress
 from logging.config import dictConfig
+from dotenv import load_dotenv
+from os import getenv
 from json import load as load_json
 from pathlib import Path
 from typing import *
 
 from static.data.version import APIVersion
-from static.data.functions import APITools, CacheTools
+from static.data.functions import APITools, LimiterTools, CacheTools
 
+
+# Load the environment variables
+load_dotenv()
 
 # Get the latest API version
-latest_api_version = APIVersion().get_latest_version()
+latest_api_version = APIVersion().latest_version
 
 # Configuration class
 class Config:
@@ -52,27 +57,35 @@ logging_config = {
 }
 dictConfig(logging_config)
 
-# Setup Flask cache
-app.config['CACHE_TYPE'] = 'simple'
+# Setup Flask limiter with Redis
+limiter = Limiter(flask_limiter_utils.get_remote_address, app=app, storage_uri=str(getenv('REDIS_URL')))
+
+# Setup Flask cache with Redis
+app.config['CACHE_TYPE'] = 'RedisCache'
+app.config['CACHE_REDIS_HOST'] = str(getenv('REDIS_HOST'))
+app.config['CACHE_REDIS_PORT'] = int(getenv('REDIS_PORT'))
+app.config['CACHE_REDIS_DB'] = int(getenv('REDIS_DB'))
+app.config['CACHE_REDIS_PASSWORD'] = str(getenv('REDIS_PASSWORD'))
+app.config['CACHE_REDIS_URL'] = str(getenv('REDIS_URL'))
 cache = Cache(app)
 
-# Setup Talisman for security headers (with custom options), CSRF protection and better compression
-limiter = Limiter(flask_limiter_utils.get_remote_address, app=app)
+# Setup Talisman for security headers (with custom options), CSRF protection and Compress for response compression
 talisman = Talisman(app, content_security_policy={'default-src': ['\'self\'', 'https://cdnjs.cloudflare.com'], 'style-src': ['\'self\'', '\'unsafe-inline\'', 'https://cdnjs.cloudflare.com'], 'script-src': ['\'self\'', 'https://cdnjs.cloudflare.com']})
 csrf = CSRFProtect(app)
 compression = Compress(app)
 
 # Setup Flask routes
 @app.route('/', methods=['GET'])
-@cache.cached(timeout=3600, make_cache_key=CacheTools.get_cache_key)
+@limiter.limit(LimiterTools.gen_ratelimit(per_sec=30))
+@cache.cached(timeout=300, make_cache_key=CacheTools.gen_cache_key)
 def initial_page() -> Any:
     return flask.render_template('index.html')
 
 
 @app.route('/api/<query_version>/parser/user-agent/', methods=['GET'])
-@cache.cached(timeout=300, make_cache_key=CacheTools.get_cache_key)
+@limiter.limit(LimiterTools.gen_ratelimit(per_sec=10, per_day=1000))
+@cache.cached(timeout=10, make_cache_key=CacheTools.gen_cache_key)
 def parser_user_agent(query_version: str) -> Any:
-    APITools.check_main_request(flask.request.remote_addr, (1, 30, 400, 1000), query_version)
     return flask.jsonify({'userAgent': flask.request.headers.get('User-Agent')})
 
 
@@ -87,4 +100,4 @@ if __name__ == '__main__':
     app.template_folder = Path(current_path, config.flask.templateFolder)
 
     # Run the web server with the specified configuration
-    app.run(host=config.flask.host, port=config.flask.port, threaded=config.flask.threadedServer, load_dotenv=config.flask.loadEnvironmentVariables, debug=config.flask.debugMode)
+    app.run(host=config.flask.host, port=config.flask.port, threaded=config.flask.threadedServer, debug=config.flask.debugMode)
