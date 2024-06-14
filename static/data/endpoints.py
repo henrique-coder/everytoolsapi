e4731b73828d7a0df427cde72cbec1153fdb987d
Henrique <henriquemoreira10fk@gmail.com>
2024-06-14 17:48:06 -0300

    Added new scrapers (Google searcher and Instagram reel downloader)

--- static/data/endpoints.py
+++ static/data/endpoints.py
@@ -1,13 +1,21 @@
 from user_agents import parse as UserAgentParser
-from urllib.parse import urlparse, parse_qs
-from re import compile as re_compile, findall
+from urllib.parse import urlparse, parse_qs, unquote, urlencode, unquote_plus
+from re import compile as re_compile, sub as re_sub, findall as re_findall
+from unicodedata import normalize
 from collections import Counter
 from langdetect import detect as detect_lang, DetectorFactory, LangDetectException
 from langcodes import Language
 from googletrans import Translator
+from googlesearch import search as google_search
+from httpx import Client as httpx_client, _exceptions as httpx_exceptions
+from fake_useragent import FakeUserAgent
 from typing import *
 
 from static.data.functions import OtherTools, APITools, LimiterTools
+
+
+# Global variables/constants
+fake_ua = FakeUserAgent()
 
 
 class APIEndpoints:
@@ -184,7 +192,7 @@ class APIEndpoints:
                     digit_counts = dict(Counter(char for char in text if char.isdigit()))
                     letter_counts = dict(Counter(char for char in text if char.isalpha()))
                     other_symbol_counts = dict(Counter(char for char in text if not char.isalnum() and not char.isspace()))
-                    word_counts = dict(Counter(findall(r'\b[a-zA-Z]+(?:\'[a-zA-Z]+)*\b', text.lower())))
+                    word_counts = dict(Counter(re_findall(r'\b[a-zA-Z]+(?:\'[a-zA-Z]+)*\b', text.lower())))
                     space_count = int(text.count(' '))
 
                     output_data['response'] = {
@@ -256,7 +264,7 @@ class APIEndpoints:
             class text_translator:
                 endpoint_url = '/tools/text-translator/'
                 allowed_methods = ['GET']
-                ratelimit = LimiterTools.gen_ratelimit_message(per_sec=4, per_day=800)
+                ratelimit = LimiterTools.gen_ratelimit_message(per_sec=4, per_day=600)
                 timeout = 1
 
                 @staticmethod
@@ -286,6 +294,165 @@ class APIEndpoints:
                         return output_data
 
                     output_data['response'] = {'translatedText': translated_text.text}
+                    output_data['api']['status'] = True
+                    output_data['api']['elapsedTime'] = start_time.stop_timer()
+
+                    return output_data
+
+        class scraper:
+            class google_search:
+                endpoint_url = '/scraper/google-search/'
+                allowed_methods = ['GET']
+                ratelimit = LimiterTools.gen_ratelimit_message(per_sec=4, per_day=400)
+                timeout = 1
+
+                @staticmethod
+                def run(request_data: Dict[str, Dict[Any, Any]]) -> dict:
+                    start_time = OtherTools.Timer()
+                    output_data = APITools.get_default_api_output_dict()
+
+                    # Request data validation
+                    if request_data['args'].get('query'): query = request_data['args']['query']
+                    else:
+                        output_data['api']['errorMessage'] = 'No "query" parameter found in the request.'
+                        return output_data
+
+                    max_results = request_data['args'].get('max_results', 10)
+                    if not max_results: max_results = 10
+
+                    try:
+                        max_results = int(max_results)
+                        if max_results < 1:
+                            output_data['api']['errorMessage'] = 'The "max_results" parameter must be greater than 0.'
+                            return output_data
+                        elif max_results > 50:
+                            output_data['api']['errorMessage'] = 'The "max_results" parameter must be less than or equal to 50.'
+                            return output_data
+                    except ValueError:
+                        output_data['api']['errorMessage'] = 'The "max_results" parameter must be an integer.'
+                        return output_data
+
+                    # Main process
+                    search_results = list()
+                    for result in google_search(query, num_results=max_results, lang='en'):
+                        search_results.append(unquote(result))
+
+                    search_results = list(set(search_results))
+
+                    output_data['response'] = {'searchResults': search_results}
+                    output_data['api']['status'] = True
+                    output_data['api']['elapsedTime'] = start_time.stop_timer()
+
+                    return output_data
+
+            class instagram_reels:
+                endpoint_url = '/scraper/instagram-reels/'
+                allowed_methods = ['GET']
+                ratelimit = LimiterTools.gen_ratelimit_message(per_sec=4, per_day=300)
+                timeout = 1
+
+                @staticmethod
+                def run(request_data: Dict[str, Dict[Any, Any]]) -> dict:
+                    start_time = OtherTools.Timer()
+                    output_data = APITools.get_default_api_output_dict()
+
+                    # Request data validation
+                    if request_data['args'].get('query'): query = request_data['args']['query']
+                    else:
+                        output_data['api']['errorMessage'] = 'No "query" parameter found in the request.'
+                        return output_data
+
+                    def is_valid_instagram_reel_url(query: AnyStr) -> bool:
+                        """
+                        Check if the URL is a valid Instagram Reels URL.
+                        :param query: URL to be checked.
+                        :return: True if the URL is a valid Instagram Reels URL, False otherwise.
+                        """
+
+                        pattern = re_compile(r'^(https?://)?(www\.)?instagram\.com(/[^/]+)?/(reel|p)/[A-Za-z0-9_-]+/?(\?.*)?$')
+                        return bool(pattern.match(query))
+
+                    if not is_valid_instagram_reel_url(query):
+                        output_data['api']['errorMessage'] = r'The URL provided is not a valid Instagram Reels URL. A valid URL should look like: https://www.instagram.com/reel/{your_reel_id}'
+                        return output_data
+
+                    # Main process
+                    def format_string(query: AnyStr, max_length: int = 128) -> str:
+                        """
+                        Format string to remove special characters and normalize it.
+                        :param query: String to be sanitized.
+                        :param max_length: Maximum length of the string (if exceeds, it will be truncated).
+                        :return: Sanitized string.
+                        """
+
+                        # Normalize string and remove special characters
+                        normalized_string = str(normalize('NFKD', query).encode('ASCII', 'ignore').decode('utf-8')).strip()
+
+                        # Remove extra spaces and special characters
+                        sanitized_string = str(re_sub(r'\s+', ' ', re_sub(r'[^a-zA-Z0-9\-_()[\]{}!$#+,. ]', str(), normalized_string)).strip())
+
+                        # Truncate string if it exceeds the maximum length
+                        if len(sanitized_string) > max_length: sanitized_string = str(sanitized_string[:max_length].rsplit(' ', 1)[0])
+
+                        # Return sanitized string
+                        return sanitized_string
+
+                    def safe_unquote_url(url: AnyStr) -> str:
+                        """
+                        Safely unquote URL.
+                        :param url: URL to be unquoted.
+                        :return: Unquoted URL.
+                        """
+
+                        parsed_url = urlparse(url)
+                        unquoted_url_base = unquote_plus(parsed_url.scheme + '://' + parsed_url.netloc + parsed_url.path)
+                        unquoted_url = str(unquoted_url_base + '?' + urlencode(parse_qs(parsed_url.query), doseq=True))
+
+                        return unquoted_url
+
+                    def edit_url_param(url: AnyStr, param: AnyStr, new_value: AnyStr) -> str:
+                        """
+                        Edit a parameter in the URL.
+                        :param url: URL to be edited.
+                        :param param: Parameter to be edited.
+                        :param new_value: New value for the parameter.
+                        :return: Edited URL with the new parameter value.
+                        """
+
+                        parsed_url = urlparse(url)
+                        query_dict = parse_qs(parsed_url.query)
+
+                        if param in query_dict: query_dict[param] = new_value
+                        else: query_dict[param] = [new_value]
+
+                        edited_url = parsed_url._replace(query=urlencode(query_dict, doseq=True)).geturl()
+
+                        return edited_url
+
+                    if not urlparse(query).scheme: query = 'https://' + query
+
+                    with httpx_client() as client:
+                        try:
+                            response = client.post('https://fastdl.app/api/convert', headers={'User-Agent': fake_ua.random, 'Accept': 'application/json'}, json={'url': query}, timeout=10)
+                        except httpx_exceptions.HTTPError:
+                            output_data['api']['errorMessage'] = 'Some error occurred in our systems during the data search. Please try again later.'
+                            return output_data
+
+                    if response.status_code != 200 or not response.json():
+                        output_data['api']['errorMessage'] = 'Some external error occurred during the data search. Please try again later.'
+                        return output_data
+
+                    response_data = response.json()
+
+                    try:
+                        filename = format_string(response_data['meta']['title']) + '.' + response_data.get('url', list(dict()))[0].get('ext', 'mp4').lower()
+                        thumbnail_url = safe_unquote_url(edit_url_param(parse_qs(urlparse(response_data['thumb']).query)['uri'][0], 'dl', '0'))
+                        media_url = safe_unquote_url(edit_url_param(parse_qs(urlparse(response_data['url'][0]['url']).query)['uri'][0], 'dl', '0'))
+                    except BaseException:
+                        output_data['api']['errorMessage'] = 'An error occurred while fetching Instagram reel data. Please try again later.'
+                        return output_data
+
+                    output_data['response'] = {'filename': filename, 'thumbnailUrl': thumbnail_url, 'mediaUrl': media_url}
                     output_data['api']['status'] = True
                     output_data['api']['elapsedTime'] = start_time.stop_timer()
 
