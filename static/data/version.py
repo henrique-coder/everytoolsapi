702506285a9ebd28568884929b62b0c47393b886
Henrique <henriquemoreira10fk@gmail.com>
2024-06-15 19:48:17 -0300

    Fixed wrong status code for invalid api calls

--- static/data/version.py
+++ static/data/version.py
@@ -25,11 +25,12 @@ class APIVersion:
         return bool(query == APIVersion().latest_version)
 
     @staticmethod
-    def send_invalid_api_version_response(query: str) -> flask.Response:
+    def send_invalid_api_version_response(query: str, status_code: int = 400) -> Tuple[flask.Response, int]:
         """
         Send an invalid API version response.
         :param query: The invalid API version.
+        :param status_code: The status code to return.
         :return: The invalid API version response.
         """
 
-        return flask.jsonify({'status': False, 'message': f'Invalid/Unsupported API version: "{query}"', 'tip': f'Use the latest API version: "{APIVersion().latest_version}"'})
+        return flask.jsonify({'status': False, 'message': f'Invalid/Unsupported API version: "{query}"', 'tip': f'Use the latest API version: "{APIVersion().latest_version}"'}), status_code
