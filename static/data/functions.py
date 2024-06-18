1d71c173af948b4bfddcb84dee51a22be133a23f
Henrique <henriquemoreira10fk@gmail.com>
2024-06-17 23:23:02 -0300

    Fixed critical error when origin requester has multiple IPs (proxies)

--- static/data/functions.py
+++ static/data/functions.py
@@ -24,6 +24,7 @@ class APITools:
         """
 
         remote_addr = request_object.environ.get('HTTP_X_FORWARDED_FOR', request_object.remote_addr)
+        if ',' in remote_addr: remote_addr = remote_addr.split(',')[0].strip()
         route = str(request_object.path)
         args = request_object.args.to_dict()
         headers = dict(request_object.headers)
